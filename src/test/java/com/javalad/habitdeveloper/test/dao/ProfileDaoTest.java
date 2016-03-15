ca1dd75c8d226f30d8754fd744508def0489e339
kotovdv <xeno93@gmail.com>
2016-03-15 23:40:49 +0300

    1. Added DBUnit usage in DAO tests 2. Changed ProfileDaoTest's tests  - from now on each test is only responsible for testing single operation

--- src/test/java/com/javalad/habitdeveloper/test/dao/ProfileDaoTest.java
+++ src/test/java/com/javalad/habitdeveloper/test/dao/ProfileDaoTest.java
@@ -1,22 +1,18 @@
 package com.javalad.habitdeveloper.test.dao;
 
-import com.javalad.habitdeveloper.dao.CheckedHabitDao;
-import com.javalad.habitdeveloper.dao.MeasuredHabitDao;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 import com.javalad.habitdeveloper.dao.ProfileDao;
-import com.javalad.habitdeveloper.domain.CheckedHabit;
-import com.javalad.habitdeveloper.domain.MeasuredHabit;
 import com.javalad.habitdeveloper.domain.Profile;
 import com.javalad.habitdeveloper.test.dao.util.AbstractDaoTest;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.junit.Before;
 import org.junit.Test;
 
 import javax.annotation.Resource;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author KotovDV
@@ -26,90 +22,64 @@ public class ProfileDaoTest extends AbstractDaoTest {
     @Resource
     private ProfileDao profileDao;
 
-    @Resource
-    private CheckedHabitDao checkedHabitDao;
-
-    @Resource
-    private MeasuredHabitDao measuredHabitDao;
-
-    private Profile profile;
-
-    @Before
-    public void beforeTest() {
-        profile = new Profile("Test name", "Test description");
-    }
-
-
-    public boolean areProfilesEqual(Profile firstProfile, Profile secondProfile){
-         return new EqualsBuilder()
-                .append(firstProfile.getId(), secondProfile.getId())
-                .append(firstProfile.getName(), secondProfile.getName())
-                .append(firstProfile.getDescription(), secondProfile.getDescription())
-                .append(firstProfile.getCheckedHabits(), secondProfile.getCheckedHabits())
-                .append(firstProfile.getMeasuredHabits(), secondProfile.getMeasuredHabits())
-                .isEquals();
-    }
-
     @Test
-    public void addAndGetTest() {
+    @ExpectedDatabase(value = "classpath:dao/ProfileDaoTest/addTest/result.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void addTest() {
+        Profile profile = new Profile("New test", "New description");
         profileDao.add(profile);
-        Profile extractedProfile = profileDao.get(profile.getId());
-        assertTrue(areProfilesEqual(profile,extractedProfile));
     }
 
     @Test
+    @DatabaseSetup(value = "classpath:dao/ProfileDaoTest/getTest/initial.xml")
+    public void getTest() {
+        Profile extractedProfile = profileDao.get(1L);
+        assertEquals(extractedProfile.getId(), 1);
+        assertEquals(extractedProfile.getName(), "getTest");
+        assertEquals(extractedProfile.getDescription(), "getTestDescription");
+    }
+
+    @Test
+    @DatabaseSetup(value = "classpath:dao/ProfileDaoTest/updateTest/initial.xml")
+    @ExpectedDatabase(value = "classpath:dao/ProfileDaoTest/updateTest/result.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
     public void updateTest() {
-        profileDao.add(profile);
-        profile.setName("Updated profile value");
-        profile.setDescription("Updated description value");
-        profileDao.update(profile);
-        Profile profileFromDb = profileDao.get(profile.getId());
-        assertTrue(areProfilesEqual(profile, profileFromDb));
+        Profile extractedProfile = new Profile("updatedName", "updatedDescription");
+        extractedProfile.setId(1L);
+        profileDao.update(extractedProfile);
     }
 
     @Test
-    public void deleteAndExistsTest() {
-        profileDao.add(profile);
-        long id = profile.getId();
-        assertTrue(id > 0);
-        assertTrue(profileDao.exists(id));
-        profileDao.delete(id);
-        assertFalse(profileDao.exists(id));
+    @DatabaseSetup(value = "classpath:dao/ProfileDaoTest/deleteTest/initial.xml")
+    @ExpectedDatabase(value = "classpath:dao/ProfileDaoTest/deleteTest/result.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void deleteTest() {
+        profileDao.delete(2L);
     }
 
     @Test
+    @DatabaseSetup(value = "classpath:dao/ProfileDaoTest/countTest/initial.xml")
+    public void countTest() {
+        assertEquals(profileDao.count(), 3);
+    }
+
+    @Test
+    @DatabaseSetup(value = "classpath:dao/ProfileDaoTest/getAllTest/initial.xml")
     public void getAllTest() {
-        Profile firstProfile = new Profile("First profile", "First profile");
-        Profile secondProfile = new Profile("Second profile", "Second profile");
-        Profile thirdProfile = new Profile("Third profile", "Third profile");
-        List<Profile> profileList = Arrays.asList(firstProfile, secondProfile, thirdProfile);
-        profileList.forEach(profile -> profileDao.add(profile));
-        long count = profileDao.count();
-        assertEquals(count, 3);
-        List<Profile> profilesFromDb = profileDao.getAll();
-        profilesFromDb.forEach(profileFromDb -> assertTrue(profileList.contains(profileFromDb)));
+        List<Profile> profiles = profileDao.getAll();
+        assertEquals(profiles.size(), 3);
+
+        profiles.forEach(profile -> {
+            long profileId = profile.getId();
+            assertTrue(profileId >= 1 && profileId <= 3);
+            assertEquals(profile.getName(), "test" + profileId);
+            assertEquals(profile.getDescription(), "description" + profileId);
+        });
     }
+
 
     @Test
-    public void getProfileHabitsTest(){
-        profileDao.add(profile);
-        long profileId = profile.getId();
-
-        List<CheckedHabit> initialCheckedHabits = Arrays.asList(new CheckedHabit("getAllTest1", "description", profileId, "* * * * * *"),
-                new CheckedHabit("getAllTest2", "description2", profileId, "* * * * * *"));
-        List<MeasuredHabit> initialMeasuredHabits = Arrays.asList(new MeasuredHabit("habit1", "description", profileId, "* * * * * *", Calendar.getInstance().getTime(), 100),
-                new MeasuredHabit("habit2", "description", profileId, "* * * * * *", Calendar.getInstance().getTime(), 100));
-
-        initialCheckedHabits.forEach(checkedHabit -> checkedHabitDao.add(checkedHabit));
-        initialMeasuredHabits.forEach(measuredHabit -> measuredHabitDao.add(measuredHabit));
-
-        Profile profileFromDb = profileDao.get(profileId);
-
-        List<CheckedHabit> extractedCheckedHabits = profileFromDb.getCheckedHabits();
-        assertTrue(extractedCheckedHabits.containsAll(initialCheckedHabits) && initialCheckedHabits.containsAll(extractedCheckedHabits));
-
-        List<MeasuredHabit> extractedMeasuredHabits = profileFromDb.getMeasuredHabits();
-        assertTrue(extractedMeasuredHabits.containsAll(initialMeasuredHabits) && initialMeasuredHabits.containsAll(extractedMeasuredHabits));
+    @DatabaseSetup(value = "classpath:dao/ProfileDaoTest/getProfileHabitsTest/initial.xml")
+    public void getProfileHabitsTest() {
+        Profile extractedProfile = profileDao.get(1L);
+        assertEquals(extractedProfile.getCheckedHabits().size(), 2);
+        assertEquals(extractedProfile.getMeasuredHabits().size(), 2);
     }
-
 }
--- src/test/java/com/javalad/habitdeveloper/test/dao/util/AbstractDaoTest.java
+++ src/test/java/com/javalad/habitdeveloper/test/dao/util/AbstractDaoTest.java
@@ -1,11 +1,13 @@
 package com.javalad.habitdeveloper.test.dao.util;
 
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 import com.javalad.habitdeveloper.configuration.DataSourceConfiguration;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -15,6 +17,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(DataSourceConfiguration.class)
-@TestExecutionListeners(value = DisableConstraintsListener.class, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+@TestExecutionListeners(value = {
+        DisableConstraintsListener.class,
+        DependencyInjectionTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class})
 public abstract class AbstractDaoTest {
+
+
+
+   /* public abstract void addTest();
+
+    public abstract void getTest();
+
+    public abstract void updateTest();
+
+    public abstract void deleteTest();
+
+    public abstract void countTest();
+
+    public abstract void getAllTest();*/
+
 }
